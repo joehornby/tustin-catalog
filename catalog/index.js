833d5bf60d33d203290f5609b45b9ee0f9d47719
Joseph Hornby <joehornby@gmail.com>
2020-10-02 16:05:30 +0100

    add design group of pages

--- catalog/index.js
+++ catalog/index.js
@@ -5,8 +5,24 @@ import { Catalog, pageLoader } from "catalog";
 const pages = [
   {
     path: "/",
-    title: "Welcome",
+    title: "Style Guide",
     content: pageLoader(() => import("./WELCOME.md"))
+  },
+  {
+    title: "Design",
+    path: "/design",
+    pages: [
+      {
+        path: "/colours",
+        title: "Colours",
+        content: pageLoader(() => import("./COLOURS.md"))
+      },
+      {
+        path: "/typography",
+        title: "Typography",
+        content: pageLoader(() => import(".TYPOGRAPHY.md"))
+      }
+    ]
   }
 ];
 
